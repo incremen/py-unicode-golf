610edf0ac65577937babfe2815fc71c460834ba3
Your Name <you@example.com>
2026-03-10 16:22:23 +0200

    background rotates during visualize animation too

--- static/js/logo_animation.js
+++ static/js/logo_animation.js
@@ -12,6 +12,8 @@ function setLogo(scale, opacity, rotate, hue) {
   el().style.setProperty('--logo-rotate', (rotate || 0) + 'deg');
   el().style.setProperty('--logo-hue', (hue || 0) + 'deg');
   document.documentElement.style.setProperty('--bg-hue', (hue || 0) + 'deg');
+  document.documentElement.style.setProperty('--bg-angle', (90 + (rotate || 0)) + 'deg');
+  document.documentElement.style.setProperty('--bg-angle-inv', (270 + (rotate || 0)) + 'deg');
 }
 
 function setLogoTransition(seconds) {
