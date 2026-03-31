d53b4ca919c231950c54c8f28caef63718292b8b
Your Name <you@example.com>
2026-03-31 23:31:47 +0300

    new color scheme for graph

--- static/graph_styles.js
+++ static/graph_styles.js
@@ -2,10 +2,10 @@ const CYTOSCAPE_STYLES = [
   {
     selector: 'node',
     style: {
-      'background-color': '#2c3e50',
-      'border-color': '#4a5568',
+      'background-color': '#ffffff',
+      'border-color': '#333',
       'border-width': 2,
-      'color': 'white',
+      'color': '#222',
       'label': 'data(label)',
       'font-family': 'Courier New',
       'font-size': '13px',
@@ -17,11 +17,11 @@ const CYTOSCAPE_STYLES = [
   },
   {
     selector: 'node.expanded',
-    style: { 'border-color': '#2ecc71', 'border-width': 3 }
+    style: { 'border-color': '#333', 'border-width': 2, 'background-color': '#f0f0f0' }
   },
   {
     selector: 'node.focused',
-    style: { 'background-color': '#1a3a5c', 'border-color': '#5dade2', 'border-width': 3 }
+    style: { 'background-color': '#ffffff', 'border-color': '#306998', 'border-width': 3 }
   },
   {
     selector: 'edge',
@@ -35,7 +35,7 @@ const CYTOSCAPE_STYLES = [
       'font-family': 'Courier New',
       'font-size': '9px',
       'color': 'data(color)',
-      'text-background-color': '#12151e',
+      'text-background-color': '#f0efe8',
       'text-background-opacity': 0.85,
       'text-background-padding': '2px',
     }
